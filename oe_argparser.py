1e87a7adb7eb1d36a312ffa7796881307a4c7c99
Guido Amoruso <guidonte@gmail.com>
2015-05-18 08:19:10 +0200

    Fix conflicting string.

--- oe_argparser.py
+++ oe_argparser.py
@@ -9,7 +9,7 @@ def oe_args_parser():
     parser.add_argument('-d', '--database', dest='database', required=True)
     parser.add_argument('-u', '--username', dest='username', default='admin')
     parser.add_argument('-p', '--password', dest='password')
-    parser.add_argument('-h', '--host', dest='host', default='localhost')
+    parser.add_argument('-H', '--host', dest='host', default='localhost')
     parser.add_argument('-P', '--port', dest='port', default='8069')
 
     return parser
